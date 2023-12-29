535f207abef5ab42fe5a53b9943eb62182cd944a
oscar-cortez-home <oscarkortez@gmail.com>
2023-12-29 16:05:17 -0400

    created navbar config file

---
+++ src/config/navbarConfig.js
@@ -0,0 +1,11 @@
+export const labels = {
+  title: "FUENTES ABIERTAS",
+  logout: {
+    value: "Cerrar sesión",
+    icon: "sign-out",
+    popupMessage: "¿Está seguro que desea cerrar sesión?",
+    popupIcon: "exclamation-triangle",
+    popupYes: "Si",
+    popupNo: "No",
+  },
+};
