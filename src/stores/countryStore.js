7f05bd3591cc6656e081376700efb31349b03b15
oscar-cortez-home <oscarkortez@gmail.com>
2024-02-20 16:04:33 -0400

    fixed countrycode to persist in sessions

--- src/stores/countryStore.js
+++ src/stores/countryStore.js
@@ -1,7 +1,20 @@
 import { create } from "zustand";
 
 // its being used here: src\layout\Dashboard\index.jsx get
+
+const getInitialState = () => {
+  const localStorageState = localStorage.getItem("countryCode");
+  return localStorageState ? JSON.parse(localStorageState) : "ES";
+};
+
+const saveToLocalStorage = (state) => {
+  localStorage.setItem("countryCode", JSON.stringify(state.code));
+};
+
 export const useCountryStore = create((set) => ({
-  code: "ES",
-  setCode: (code) => set({ code }),
+  code: getInitialState(),
+  setCode: (code) => {
+    saveToLocalStorage({ code });
+    set({ code });
+  },
 }));
